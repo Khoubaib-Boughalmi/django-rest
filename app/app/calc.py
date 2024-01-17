43be814f816f4175cfc8f53cf80d0ee3d32e2012
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-01-17 12:48:06 +0100

    created first unit test example

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+    A simple calculator func
+"""
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class TestCalc(SimpleTestCase):
+    """Test the add function from calc.py"""
+    def test_add(self):
+        res = calc.add(3, 8)
+        self.assertEqual(res, 11)
